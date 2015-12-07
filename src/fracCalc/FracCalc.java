bf531d1fb67f77666e0262b1ad6afef001ea25a3
Haley <Haley.boyles@opsknights.com>
2015-12-07 09:05:02 -0500

    kinda working

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -15,35 +15,57 @@ public class FracCalc {
 
     }
     
+    public static String[] splitfraction(String input)
+    {String frac=input;
+	String whole="";
+if (frac.contains("_"))
+	{ whole=frac.substring(0,frac.indexOf("_"));}
+else{
+	  whole="0";}
+String num="";
+String den="";
+if (frac.contains("/"))
+	{num=frac.substring(frac.indexOf("_")+1,frac.indexOf("/"));
+	den=frac.substring(frac.indexOf("/")+1);}
+    String[]splitfraction = {whole,num,den};
+    return splitfraction;}
+
+    public static int reduce (int a,int b){
+		if (a == 0){
+		return b;}
+		while(b != 0){
+			if(a>b){
+				a=a-b;
+			else
+				b=b-a;}
+    	return a;}
+		return "a/b";}
+    
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
-    //
     // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
     //      e.g. input ==> "1/2 + 3/4"
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String scanned)
+    public static Integer produceAnswer(String scanned)
     { 
-    	String whole="";
-    	String second = scanned.substring(scanned.lastIndexOf(" ")+1);
-    	if (second.contains("_"))
-    		{ whole=second.substring(0,second.indexOf("_"));}
-    	else{
-    		  whole="0";}
-    	String num2="";
-    	String den2="";
-    	if (second.contains("/"))
-    		{num2=second.substring(second.indexOf("_")+1,second.indexOf("/"));
-    		den2=second.substring(second.indexOf("/")+1);}
-    	else{
-    			whole=second.substring(second.indexOf("_")+1);
-    			den2="1";
-    			num2="0";                   
-    			
-    		}
     	
-    return "whole:"+ whole + " numerator:"+ num2 + " denominator:" + den2 ;
+    	String second = scanned.substring(scanned.lastIndexOf(" ")+1);
+    	String first = scanned.substring(0,(scanned.indexOf(" ")));
+    	String sign = scanned.substring(scanned.indexOf(" ")+1,scanned.lastIndexOf(" "));
+    	String[] secondsplit = splitfraction(second);
+    	String[] firstsplit = splitfraction(first);
+    	Integer whole = Integer.valueOf(firstsplit[0])+Integer.valueOf(secondsplit[0]);
+    	Integer den = Integer.valueOf(firstsplit[2])*Integer.valueOf(secondsplit[2]);
+        Integer top1 = Integer.valueOf(firstsplit[1])*Integer.valueOf(secondsplit[2]);
+        Integer top2 = Integer.valueOf(firstsplit[2])*Integer.valueOf(secondsplit[1]);
+    	int num = top1+top2;
+    	int reduce = reduce(num,den);
+    	
+    	
+    	
+    return ;
         
 		// TODO Auto-generated method stub
 	}}
