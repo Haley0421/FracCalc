3562a07647bbe5cc8815eb0612417871c8fb5d30
Haley <Haley.boyles@opsknights.com>
2015-12-08 21:25:01 -0500

    I dont know what is wrong...

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -30,17 +30,20 @@ if (frac.contains("/"))
     String[]splitfraction = {whole,num,den};
     return splitfraction;}
 
-    public static int reduce (int a,int b){
-		if (a == 0){
-		return b;}
-		while(b != 0){
-			if(a>b){
-				a=a-b;
-			else
-				b=b-a;}
-    	return a;}
-		return "a/b";}
-    
+    public static int greatestcommon (int num,int den1){
+    	while (den1 > 0){
+    		int x = den1;
+    		den1 = num % den1;
+    		num = x;
+    	}
+    	return num;
+    }
+    public static int[] reduce(int num, int den){
+    	int greatest = greatestcommon(num, den);
+    	int[]reduced = {num/greatest, den/greatest};
+    	return reduced;
+    }
+		
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
     // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
@@ -48,9 +51,9 @@ if (frac.contains("/"))
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static Integer produceAnswer(String scanned)
+    public static String produceAnswer(String scanned)
     { 
-    	
+    	int num = 0;
     	String second = scanned.substring(scanned.lastIndexOf(" ")+1);
     	String first = scanned.substring(0,(scanned.indexOf(" ")));
     	String sign = scanned.substring(scanned.indexOf(" ")+1,scanned.lastIndexOf(" "));
@@ -60,15 +63,37 @@ if (frac.contains("/"))
     	Integer den = Integer.valueOf(firstsplit[2])*Integer.valueOf(secondsplit[2]);
         Integer top1 = Integer.valueOf(firstsplit[1])*Integer.valueOf(secondsplit[2]);
         Integer top2 = Integer.valueOf(firstsplit[2])*Integer.valueOf(secondsplit[1]);
-    	int num = top1+top2;
-    	int reduce = reduce(num,den);
-    	
-    	
-    	
-    return ;
-        
-		// TODO Auto-generated method stub
-	}}
+    	int div=0;
+    	int top = 0;
+        if (sign == "+")
+    			top = top1+top2;
+    	if (sign == "*")
+    		top = top1*top2;
+    	if (sign == "/"){
+    		top = top1;
+    		den = top2;}
+    	if (sign == "-")
+    		top = top1-top2;
+    	if (top % den==0) {
+    		whole += top/den;
+    		top=0;
+    		den=0;
+    	}
+    		
+   
+    	int[] reduce = reduce(top,den);  	
+    	String fract = "";
+    	if (reduce[0]==0)
+    		fract = "" + whole;
+    	if (whole == 0)
+    		fract = reduce[0] + "/" + reduce[1];
+    	if (whole==0 & reduce[0]==0)
+    		fract="0";
+    	if (whole != 0 & reduce[0]!=0){
+    		fract= whole + "_" + reduce[0] + "/" + reduce[1];
+		// TODO Auto-generated method stu
+		}
+		return fract;}}
 
     // TODO: Fill in the space below with any helper methods that you think you will need
     
